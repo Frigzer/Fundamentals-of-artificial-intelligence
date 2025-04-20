dd9d7a42fb6fc5b15c8325d5febdd77e3ea14be2
Adam Borek <borekadam89@gmail.com>
2025-04-20 21:59:38 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from model import ASLClassifier
+from train import train_model
+from serialize_data import get_serialized_dataloaders
+import torch
+
+if __name__ == "__main__":
+    train_loader, val_loader, test_loader = get_serialized_dataloaders(batch_size=32)
+    model = ASLClassifier(num_classes=36)
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    train_model(model, train_loader, val_loader, epochs=10, lr=0.001, device=device)
